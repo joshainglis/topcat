895ce8c250f755b4817127e8ac90e3b551c169f1
Josha Inglis <josha@jci.dev>
2025-04-23 18:08:31 +1000

    move overlay

--- flake.nix
+++ flake.nix
@@ -5,26 +5,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-          overlays = [
-            naersk.outputs.overlay
-          ];
-        };
-      in
-      {
-        defaultPackage = pkgs.naersk.buildPackage { src = ./.; };
+  outputs = inputs@{ self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+            overlays = [
+              inputs.naersk.outputs.overlay
+            ];
+          };
+        in
+        {
+          defaultPackage = pkgs.naersk.buildPackage { src = ./.; };
 
-        overlays.default = (final: prev: {
-          topcat = prev.naersk.buildPackage.buildPackage { src = ./.; };
-        });
-
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-      }
-    );
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        }
+      ) //
+    {
+      overlays.default = (final: prev: {
+        naersk = prev.callPackage inputs.naersk { };
+        topcat = final.naersk.buildPackage.buildPackage { src = ./.; };
+      });
+    };
 }
