f812a8a3a22dae18cb5244d926f8942f930b1733
Josha Inglis <joshainglis@gmail.com>
2025-10-31 15:15:00 +1100

    Add Rust toolchain and dev environment to `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,28 +1,70 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = (import nixpkgs) {
-            inherit system;
-          };
-        in
-        {
-          defaultPackage = pkgs.callPackage ./package.nix { };
-
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ rustc cargo ];
-          };
-        }
-      ) //
+  outputs =
     {
-      overlays.default = (final: prev: {
-        topcat = prev.callPackage ./package.nix { };
-      });
+      self,
+      flake-utils,
+      rust-overlay,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+          config.allowUnfree = true;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable."1.88.0".default.override {
+          extensions = [
+            "rust-src"
+            "clippy"
+            "rustfmt"
+          ];
+        };
+
+      in
+      {
+        defaultPackage = pkgs.callPackage ./package.nix { };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            rustToolchain
+          ];
+
+          buildInputs =  [
+            pkgs.openssl
+            pkgs.pkg-config
+
+            pkgs.rust-analyzer
+            pkgs.jetbrains.rust-rover
+          ];
+
+          shellHook = ''
+            mkdir -p ~/.rust-rover/toolchain
+
+            ln -sfn ${rustToolchain}/lib ~/.rust-rover/toolchain
+            ln -sfn ${rustToolchain}/bin ~/.rust-rover/toolchain
+
+            export RUST_SRC_PATH="$HOME/.rust-rover/toolchain/lib/rustlib/src/rust/library"
+          '';
+        };
+      }
+    )
+    // {
+      overlays.default = (
+        final: prev: {
+          topcat = prev.callPackage ./package.nix { };
+        }
+      );
     };
 }
